e186ea9d393de0916fa4c294a4bd5b2eda41d558
csheng91 <csheng00@gmail.com>
2018-05-05 21:58:32 -0400

    added basic structure for header

--- frontend/src/ui/components/Board.jsx
+++ frontend/src/ui/components/Board.jsx
@@ -4,12 +4,12 @@ import '../css/Board.css';
 
 const Board = ({ children }) => (
   <div className="col s12 m8 offset-m2 center-align p-0" >
-    <div className="fill-width" style={{width: 100 + "%", backgroundImage: "url(/assets/board2.svg)"}}>
+    <div className="fill-width board-back">
       <div className="flexCont" >
         {children}
       </div>  
       <div className="gridLines" >
-        <img src="/assets/grid.svg" style={{width: 100 + "%"}} alt="grid lines" />
+        <img src="/assets/grid.svg" className="fill-width" alt="grid lines" />
       </div>
     </div>
   </div>
--- frontend/src/ui/components/Header.jsx
+++ frontend/src/ui/components/Header.jsx
@@ -4,14 +4,14 @@ import '../css/Header.css';
 
 const Header = () => (
   <header className="flex-header" >
-    <span style={{width: 25 + "%"}} >
-                <img src={"/assets/players/P1.svg"} style={{maxWidth: 30 + "%"}} />
-                <h4 >You</h4>
-            </span>
-            <span style={{width: 25 + "%"}} >
-                <img src={"/assets/players/P1.svg"} style={{maxWidth: 30 + "%"}} />
-                <h4 >Active Player</h4>
-            </span>
+    <span className="center-align" >
+      <img src={"/assets/players/P1.svg"} className="header-content" />
+      <p className="center-align" >You</p>
+    </span>
+    <span className="center-align" >
+      <img src={"/assets/players/P1.svg"} className="header-content" />
+      <p className="center-align" >Active Player</p>
+    </span>
   </header>
 );
 
